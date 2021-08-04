01b1f86b7f508fa507f29d24f84b0af8b9db8f1e
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-08-04 23:45:41 +0900

    신규 아이디 추천 - 정규표현식 이용

--- Programmers/NewIdRec/src/Main.java
+++ Programmers/NewIdRec/src/Main.java
@@ -65,3 +65,29 @@ class Solution {
         return answer;
     }
 }
+
+// 정규표현식 이용
+//class Solution {
+//    public String solution(String new_id) {
+//        String answer = "";
+//        String temp = new_id.toLowerCase();
+//
+//        temp = temp.replaceAll("[^-_.a-z0-9]","");
+//        System.out.println(temp);
+//        temp = temp.replaceAll("[.]{2,}",".");
+//        temp = temp.replaceAll("^[.]|[.]$","");
+//        System.out.println(temp.length());
+//        if(temp.equals(""))
+//            temp+="a";
+//        if(temp.length() >=16){
+//            temp = temp.substring(0,15);
+//            temp=temp.replaceAll("^[.]|[.]$","");
+//        }
+//        if(temp.length()<=2)
+//            while(temp.length()<3)
+//                temp+=temp.charAt(temp.length()-1);
+//
+//        answer=temp;
+//        return answer;
+//    }
+//}
