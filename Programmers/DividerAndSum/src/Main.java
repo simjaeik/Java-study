eb5a45c42c3dbfffbee1d83d92c0503aa779c2cf
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-08-10 12:15:34 +0900

    [Programmers] (Level_1) 약수의 개수와 덧셈
    - 나는 인수분해를 이용해서 풀엇다.
    - 다른 사람들은 set이나 배열을 통해 풀었는데, 이 방법도 괜찮은것 같다.

---
+++ Programmers/DividerAndSum/src/Main.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        Solution s = new Solution();
+        System.out.println(s.solution(1, 2));
+    }
+}
+
+class Solution {
+    public int solution(int left, int right) {
+        int answer = 0;
+
+        for(int num = left; num<=right; num++){
+            int cnt = getDivideCount(num);
+            if(cnt%2==0){
+                answer+=num;
+            }
+            else{
+                answer-=num;
+            }
+        }
+
+        return answer;
+    }
+
+    public int getDivideCount(int num){
+        int answer = 1;
+
+        int len = num/2;
+        for(int i = 2; i<=len; i++){
+            int top = 0;
+            while(num%i==0 && num > 1){
+                num /= i;
+                top++;
+            }
+            answer *= top+1;
+            if(num <= 1 ) break;
+        }
+        if(num!=1 && answer==1)
+            return 2;
+
+        return answer;
+    }
+}
