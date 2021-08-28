2353b55d062968a98ee6bf9430a9983a36bddddd
JaeIk <43948654+simjaeik@users.noreply.github.com>
2021-08-28 15:29:38 +0900

    [BaekJoon] (실버 5) 크로아티아 알파벳 변환 - 2941
    - 단순 문자열 변환

--- Programmers/SearchRanking/src/Main.java
+++ Programmers/SearchRanking/src/Main.java
@@ -1,6 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
+import java.sql.Array;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -14,56 +13,125 @@ public class Main {
 }
 
 class Solution {
+    static TreeMap<String, ArrayList<Integer>> hs = new TreeMap<>();
+
     public ArrayList<Integer> solution(String[] info, String[] query) {
         ArrayList<Integer> answer = new ArrayList<>();
 
         ArrayList<Man> list = new ArrayList<>();
 
-        for(String line : info){
-            list.add(new Man(line));
-        }
+        getHashMap(info);
 
         Collections.sort(list);
 
-        for(String line : query){
-            int cnt = 0;
-            int num = Integer.parseInt(line.replaceAll("[^0-9]",""));
-            for(int i=0; i<list.size(); i++){
-                Man cur = list.get(i);
-                if(num > cur.score) break;
-                if(cur.checkValid(line)) cnt++;
-            }
-            answer.add(cnt);
+        for (String line : query) {
+            String [] token = line.split(" ");
+            String [] tok = {token[0], token[2], token[4], token[6]};
+            int target = Integer.parseInt(token[7]);
+
+            String str = getStr(tok);
+
+            ArrayList<Integer> li = hs.get(str);
+            Collections.sort(li);
+
+            answer.add(getCount(li, target, 0, li.size()-1));
         }
 
         return answer;
     }
 
-    public class Man implements Comparable<Man>{
+    public int getCount(ArrayList<Integer> li, int target, int left, int right){
+        int mid = (left+right)/2;
+
+        int cur = li.get(mid);
+
+        if(left > right) {
+            return mid+1;
+        }
+        else if(cur <= target){
+            return getCount(li, target, mid+1, right);
+        }
+        else {
+            return getCount(li, target, left, mid-1);
+        }
+
+    }
+
+    public void getHashMap(String[] info) {
+        for (String line : info) {
+            String[] token = line.split(" ");
+            String str = getStr(token);
+            int num = Integer.parseInt(token[4]);
+
+            allSynario("", str, 0, num);
+        }
+    }
+
+    public void allSynario(String cur, String str, int idx, int num){
+        if(idx==str.length()){
+            if (!hs.containsKey(cur)) {
+                hs.put(cur, new ArrayList<>());
+            }
+            hs.get(cur).add(num);
+            return;
+        }
+
+        allSynario(cur+"-", str, idx+1, num);
+        allSynario(cur+str.charAt(idx), str, idx+1, num);
+
+    }
+
+    public String getStr(String [] token){
+        String ans = "";
+        String str = token[0];
+        if(str.equals("java")) ans+="j";
+        else if(str.equals("python")) ans+="p";
+        else if (str.equals("cpp")) ans+="c";
+        else ans+="-";
+
+        str = token[1];
+        if(str.equals("backend")) ans+="b";
+        else if (str.equals("frontend")) ans+="f";
+        else ans+="-";
+
+        str = token[2];
+        if(str.equals("junior")) ans+="j";
+        else if (str.equals("senior")) ans+="s";
+        else ans+="-";
+
+        str = token[3];
+        if(str.equals("pizza")) ans+="p";
+        else if(str.equals("chicken")) ans+="c";
+        else ans+="-";
+
+        return ans;
+    }
+
+    public class Man implements Comparable<Man> {
         String[] conditions = new String[4];
         int score;
 
-        Man(String line){
+        Man(String line) {
             String[] token = line.split(" ");
-            for(int i=0; i<4;i++) this.conditions[i] = token[i];
+            for (int i = 0; i < 4; i++) this.conditions[i] = token[i];
             this.score = Integer.parseInt(token[4]);
         }
 
-        boolean checkValid(String cmd){
+        boolean checkValid(String cmd) {
             String[] token = cmd.split(" and ");
-            for(int i=0; i<3;i++){
-                if(token[i].equals("-")) continue;
-                if(!token[i].equals(this.conditions[i])) return false;
+            for (int i = 0; i < 3; i++) {
+                if (token[i].equals("-")) continue;
+                if (!token[i].equals(this.conditions[i])) return false;
             }
 
             String[] last = token[3].split(" ");
             int sc = 0;
 
-            if(last.length==2) {
-                if(!last[0].equals("-") && !last[0].equals(this.conditions[3])) return false;
+            if (last.length == 2) {
+                if (!last[0].equals("-") && !last[0].equals(this.conditions[3])) return false;
                 sc = Integer.parseInt(last[1]);
-            }else{
-                sc =  Integer.parseInt(last[0]);
+            } else {
+                sc = Integer.parseInt(last[0]);
             }
 
             return sc <= this.score;
